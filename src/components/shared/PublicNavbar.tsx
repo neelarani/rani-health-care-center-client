1e2ee4504141af5a5732b4faf69e1a71e9ef4e64
neelarani <neela9624@gmail.com>
2025-11-11 10:23:29 +0600

    refactor(Navbar): Refactor Navbar

--- src/components/shared/PublicNavbar.tsx
+++ src/components/shared/PublicNavbar.tsx
@@ -1,12 +1,9 @@
+import { Menu } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '../ui/sheet';
-import { Menu } from 'lucide-react';
-
-import Image from 'next/image';
-import logo from '../../assets/logo/logo.png';
-import { getCookie } from '@/services/auth/tokenHandler';
 import LogoutButton from './LogoutButton';
+import { getCookie } from '@/services/auth/tokenHandler';
 
 const PublicNavbar = async () => {
   const navItems = [
@@ -17,13 +14,13 @@ const PublicNavbar = async () => {
     { href: '#', label: 'NGOs' },
   ];
 
-  const accessToken = await getCookie('accesstoken');
+  const accessToken = await getCookie('accessToken');
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur  dark:bg-background/95">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link href="/" className="flex items-center space-x-2">
-          <Image src={logo} height={200} width={200} alt="logo" />
+          <span className="text-xl font-bold text-primary">PH Doc</span>
         </Link>
 
         <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
@@ -42,7 +39,7 @@ const PublicNavbar = async () => {
           {accessToken ? (
             <LogoutButton />
           ) : (
-            <Link href={'/login'} className="text-lg font-medium">
+            <Link href="/login">
               <Button>Login</Button>
             </Link>
           )}
@@ -71,15 +68,10 @@ const PublicNavbar = async () => {
                   </Link>
                 ))}
                 <div className="border-t pt-4 flex flex-col space-y-4">
-                  <div className=" items-center space-x-2">
-                    {accessToken ? (
-                      <LogoutButton />
-                    ) : (
-                      <Link href={'/login'} className="text-lg font-medium">
-                        <Button>Login</Button>
-                      </Link>
-                    )}
-                  </div>
+                  <div className="flex justify-center"></div>
+                  <Link href="/login" className="text-lg font-medium">
+                    <Button>Login</Button>
+                  </Link>
                 </div>
               </nav>
             </SheetContent>
