7d2ffb90af142b2a01d6771f3403c3be1af5b176
neelarani <neela9624@gmail.com>
2025-12-17 18:01:59 +0600

    feat: optimize admin dashboard, patient dashboard and doctor dashboard

---
+++ src/app/(dashboardLayout)/doctor/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+import { DashboardSkeleton } from '@/components/shared/DashboardSkeleton';
+
+const DoctorDashboardLoading = () => {
+  return <DashboardSkeleton />;
+};
+
+export default DoctorDashboardLoading;
